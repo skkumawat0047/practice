8cf5dcab2ef2e9f8dbece4d98cfc6f8cdca3be85
student skkumawat0047 <sk.kumawat0047@gmail.com>
2025-09-14 00:49:09 +0530

    add fiiless

--- practice.cpp
+++ practice.cpp
@@ -6,16 +6,16 @@ class Student {
     string name;
 
 public:
-    // Normal constructor
+
     Student(int a, string n) {
         age = a;
         name = n;
     }
 
-    // Copy constructor
+
     Student(const Student &s) {
-        age = s.age;     // copy age
-        name = s.name;   // copy name
+        age = s.age;    
+        name = s.name;   
         cout << "Copy constructor called!" << endl;
     }
 
@@ -25,9 +25,10 @@ public:
 };
 
 int main() {
-    Student s1(20, "Alice");  // create object s1
-    Student s2 = s1;          // copy s1 into s2
+    Student s1(20, "Alice");
+    Student s2 = s1;       
 
     s1.display();
+
     s2.display();
 }
--- sum.cpp
+++ sum.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
+#include<unordered_map>
 using namespace std;
 int main(){
-    int a =3;
-    int b=3;
-    cout<<a+b<<endl;    
+    unordered_map<int, string>m;
+    m.insert(45,"rohit");
+    m["sanjay"]=6;
+    cout<<m["sanjay"]<<endl;
 }
