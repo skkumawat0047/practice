95cb7b833cd4d7f83695ad8942cfc6d941f2ff98
student skkumawat0047 <sk.kumawat0047@gmail.com>
2025-09-21 18:30:22 +0530

    linklist in c

--- practice.c
+++ practice.c
@@ -1,38 +1,29 @@
 #include<stdio.h>
-void swap(int *a, int *b){
-    int temp = *a;
-    *a=*b;
-    *b=temp;
-}
-int pivit(int arr[], int st, int end){
-    int idx=st, pivot=arr[st];
-    for(int i=st+1; i<=end; i++){
-        if(arr[i]<pivot){
-            idx++;
-            swap(&arr[i], &arr[idx]);
-        }
-    }
-    swap(&arr[idx],&arr[st]);
-    return idx;
-}
-void quicksort(int arr[], int st, int end){
-    if(st<end){
-        int pivitidx = pivit(arr, st, end);
-        quicksort(arr,st, pivitidx-1);
-        quicksort(arr, pivitidx+1, end);
-    }
-}
+#include<stdlib.h>
+struct Node
+{
+    int data;
+     struct Node* next;    
+};
+
+struct Node* createNode(int val)
+{
+    struct Node* newNode=(struct Node*)malloc(sizeof(struct Node));
+    newNode->data=val;
+    newNode->next=NULL;
+    return newNode;
+};
+
+
 int main(){
-    int n;
-    printf("enter a number n:");
-    scanf("%d" ,&n);
-    int arr[n];
-    for(int i=0; i<n; i++){
-        scanf("%d", &arr[i]);
+    struct Node* head=createNode(10);
+    head->next=createNode(13);
+    head->next->next=createNode(16);
+    struct Node* temp=head;
+    while (temp!=NULL)
+    {
+        printf(" %d ",temp->data);
+        temp=temp->next;
     }
-    quicksort(arr, 0, n-1);
-    for(int i=0; i<n; i++){
-        printf(" %d ", arr[i]);
-    }
-    return 0;
+    
 }
