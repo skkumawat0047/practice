944f4bf58f898e68410fca8be6bafeabe4c67ab9
student skkumawat0047 <sk.kumawat0047@gmail.com>
2025-09-21 22:03:07 +0530

    complete function of linklist

--- practice.c
+++ practice.c
@@ -3,48 +3,90 @@
 struct Node
 {
     int data;
-    struct Node* next;
+    struct Node*next;
 };
 
 struct Node* head=NULL;
+struct Node* createNode(int val)
+{
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data=val;
+    newNode->next=NULL;
+    return newNode;
+};
 
-struct Node* createNode(int val){
-    struct Node* newnode=(struct Node*)malloc(sizeof(struct Node));
-    newnode->data=val;
-    newnode->next=NULL;
-    return newnode;
-}
-void addNode(int val){
-    struct Node* newNode=createNode(val);
+struct Node* addivalue(int val){
+    struct Node* value = createNode(val);
     if(head==NULL){
-        head=newNode;
+        head=value;
     }else{
-        struct Node*temp=head;
+        struct Node* temp=head;
         while (temp->next!=NULL)
         {
             temp=temp->next;
         }
-        temp->next=newNode;
+        temp->next=value;
     }
-}
+};
 void printall(){
+    struct Node*temp=head;
+    while (temp!=NULL)
+    {
+        printf("%d ", temp->data);
+        temp=temp->next;
+    }
+    printf("\n");
+}
+void printfront(){
+    printf("%d ", head->data);
+    printf("\n");
+
+}
+void popfront(){
+    struct Node*temp=head;
+    head=head->next;
+    temp->next=NULL;
+    printf("%d ", temp->data);
+    free(temp);
+    printf("\n");
+}
+void popback(){
+    if (head == NULL) return;
+    if (head->next == NULL) {
+        free(head);
+        head = NULL;
+        return;
+    }
+    struct Node* temp = head;
+    struct Node* prev = NULL;
+    while (temp->next != NULL) {
+        prev = temp;
+        temp = temp->next;
+    }
+    prev->next = NULL;
+    free(temp);
+}
+void addfront(int val){
+    struct Node* addFront=createNode(val);
     if(head==NULL){
-        printf("linklist is empty!");
+        head=addFront;
     }else{
         struct Node*temp=head;
-        while (temp!=NULL)
-        {
-            printf("%d ", temp->data);
-            temp=temp->next;
-        }
-        
+        temp=addFront;
+        temp->next=head;
+        head=temp;
     }
+    
 }
 int main(){
-    addNode(3);
-    addNode(13);
-    addNode(38);
-    addNode(32);
-    addNode(49);
+    addivalue(2);
+    addivalue(3);
+    addivalue(4);
+    printall();
+    addfront(12);
+    printall();
+    printfront();
+    popfront();
+    popback();
     printall();
 }
