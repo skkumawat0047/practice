cda7a859d15972eca0797e4dac1c5e3ba6bba6d6
student skkumawat0047 <sk.kumawat0047@gmail.com>
2025-09-21 19:22:39 +0530

    made new file own me

--- practice.c
+++ practice.c
@@ -1,49 +1,50 @@
 #include<stdio.h>
 #include<stdlib.h>
-
 struct Node
 {
     int data;
     struct Node* next;
 };
 
-struct Node* createNode(int val){
-    struct Node* newNode= (struct Node*)malloc(sizeof(struct Node));
-    newNode->data=val;
-    newNode->next=NULL;
-    return newNode;
-};
-
 struct Node* head=NULL;
-void addnode(int val){
-    struct Node* New=createNode(val);
+
+struct Node* createNode(int val){
+    struct Node* newnode=(struct Node*)malloc(sizeof(struct Node));
+    newnode->data=val;
+    newnode->next=NULL;
+    return newnode;
+}
+void addNode(int val){
+    struct Node* newNode=createNode(val);
     if(head==NULL){
-        head=New;
-    }
-    else{
-        struct Node* temp=head;
+        head=newNode;
+    }else{
+        struct Node*temp=head;
         while (temp->next!=NULL)
         {
             temp=temp->next;
         }
-        temp->next=New;
+        temp->next=newNode;
     }
 }
 void printall(){
     if(head==NULL){
-        printf("the linklist is empty! ");
-        return;
-    }
-    struct Node* temp=head;
-    while (temp!=NULL)
-    {
-        printf("%d ",temp->data);
-        temp=temp->next;
+        printf("linklist is empty!");
+    }else{
+        struct Node*temp=head;
+        while (temp!=NULL)
+        {
+            printf("%d ", temp->data);
+            temp=temp->next;
+        }
+        
     }
 }
 int main(){
-    addnode(10);
-    addnode(36);
-    addnode(21);
+    addNode(3);
+    addNode(13);
+    addNode(38);
+    addNode(32);
+    addNode(49);
     printall();
 }
