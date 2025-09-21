13670c443c6ecd4436746ee7f73318a2080a4125
student skkumawat0047 <sk.kumawat0047@gmail.com>
2025-09-21 19:03:38 +0530

    add function of createNode

--- practice.c
+++ practice.c
@@ -1,29 +1,49 @@
 #include<stdio.h>
 #include<stdlib.h>
+
 struct Node
 {
     int data;
-     struct Node* next;    
+    struct Node* next;
 };
 
-struct Node* createNode(int val)
-{
-    struct Node* newNode=(struct Node*)malloc(sizeof(struct Node));
+struct Node* createNode(int val){
+    struct Node* newNode= (struct Node*)malloc(sizeof(struct Node));
     newNode->data=val;
     newNode->next=NULL;
     return newNode;
 };
 
-
-int main(){
-    struct Node* head=createNode(10);
-    head->next=createNode(13);
-    head->next->next=createNode(16);
+struct Node* head=NULL;
+void addnode(int val){
+    struct Node* New=createNode(val);
+    if(head==NULL){
+        head=New;
+    }
+    else{
+        struct Node* temp=head;
+        while (temp->next!=NULL)
+        {
+            temp=temp->next;
+        }
+        temp->next=New;
+    }
+}
+void printall(){
+    if(head==NULL){
+        printf("the linklist is empty! ");
+        return;
+    }
     struct Node* temp=head;
     while (temp!=NULL)
     {
-        printf(" %d ",temp->data);
+        printf("%d ",temp->data);
         temp=temp->next;
     }
-    
+}
+int main(){
+    addnode(10);
+    addnode(36);
+    addnode(21);
+    printall();
 }
