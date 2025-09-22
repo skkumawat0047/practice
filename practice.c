9c66dbc6d6c4a62ae799a5317f73ca45042dfde4
student skkumawat0047 <sk.kumawat0047@gmail.com>
2025-09-22 21:44:38 +0530

    add node

--- practice.c
+++ practice.c
@@ -1,92 +1,34 @@
 #include<stdio.h>
 #include<stdlib.h>
-struct Node
-{
+struct Node{
     int data;
-    struct Node*next;
+    struct Node* next;
 };
 
-struct Node* head=NULL;
-struct Node* createNode(int val)
-{
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data=val;
-    newNode->next=NULL;
-    return newNode;
-};
-
-struct Node* addivalue(int val){
-    struct Node* value = createNode(val);
-    if(head==NULL){
-        head=value;
-    }else{
-        struct Node* temp=head;
-        while (temp->next!=NULL)
-        {
-            temp=temp->next;
-        }
-        temp->next=value;
-    }
-};
-void printall(){
-    struct Node*temp=head;
+int main(){
+    struct Node* head=(struct Node*)malloc(sizeof(struct Node));
+    struct Node* head2=(struct Node*)malloc(sizeof(struct Node));
+    struct Node* head3=(struct Node*)malloc(sizeof(struct Node));
+    struct Node* head4=(struct Node*)malloc(sizeof(struct Node));
+    head->data=9;
+    head2->data=12;
+    head3->data=18;
+    head4->data=13;
+    head->next=head2;
+    head2->next=head3;
+    head3->next=head4;
+    head4->next=NULL;
+    struct Node* newnode=(struct Node*)malloc(sizeof(struct Node));
+    struct Node* temp=(struct Node*)malloc(sizeof(struct Node));
+    // struct Node* next=temp->next;
+    temp->data= 7;
+    temp->next = head3;
+    head2->next = temp;
+    temp=head;
     while (temp!=NULL)
     {
-        printf("%d ", temp->data);
+        printf("%d->",temp->data);
         temp=temp->next;
     }
-    printf("\n");
-}
-void printfront(){
-    printf("%d ", head->data);
-    printf("\n");
-
-}
-void popfront(){
-    struct Node*temp=head;
-    head=head->next;
-    temp->next=NULL;
-    printf("%d ", temp->data);
-    free(temp);
-    printf("\n");
-}
-void popback(){
-    if (head == NULL) return;
-    if (head->next == NULL) {
-        free(head);
-        head = NULL;
-        return;
-    }
-    struct Node* temp = head;
-    struct Node* prev = NULL;
-    while (temp->next != NULL) {
-        prev = temp;
-        temp = temp->next;
-    }
-    prev->next = NULL;
-    free(temp);
-}
-void addfront(int val){
-    struct Node* addFront=createNode(val);
-    if(head==NULL){
-        head=addFront;
-    }else{
-        struct Node*temp=head;
-        temp=addFront;
-        temp->next=head;
-        head=temp;
-    }
-    
-}
-int main(){
-    addivalue(2);
-    addivalue(3);
-    addivalue(4);
-    printall();
-    addfront(12);
-    printall();
-    printfront();
-    popfront();
-    popback();
-    printall();
+    printf("NULL\n");
 }
